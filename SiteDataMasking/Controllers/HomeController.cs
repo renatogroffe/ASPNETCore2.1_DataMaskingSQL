76a3884e41b9694827a8e02d04eb034119b0d267
Renato Groffe <renato.groffe@yahoo.com.br>
2018-11-05 00:04:18 -0200

    ASP.NET Core 2.1 + Dapper + SQL Server + Data Masking
    ASP.NET Core 2.1 + Dapper + SQL Server + Data Masking

---
+++ SiteDataMasking/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+using SiteDataMasking.Models;
+
+namespace SiteDataMasking.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(
+            [FromServices]IConfiguration config)
+        {
+            List<Cliente> clientes;
+
+            using (SqlConnection conexao =
+                new SqlConnection(
+                    config.GetConnectionString("ExemploDataMask")))
+            {
+                clientes = conexao.Query<Cliente>(
+                    "SELECT * " +
+                    "FROM dbo.Clientes").AsList();
+            }
+
+            return View(clientes);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
